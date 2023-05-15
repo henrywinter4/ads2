3776def5bbf2d191b39bb3f8c9cb4c6a3b3ed290
henrywinter4 <110010671+henrywinter4@users.noreply.github.com>
2023-05-15 21:35:57 +0930

    BONGUS!!!!!!

---
+++ Autocomplete.h
@@ -0,0 +1,16 @@
+#ifndef AUTOCOMPLETE_H
+#define AUTOCOMPLETE_H
+#include "Trie.h"
+#include <string>
+#include <vector>
+class Trie;
+class Autocomplete{
+    private:
+        Trie t;
+    public:
+        Autocomplete();
+        void insert(std::string value);
+        std::vector<std::string> getSuggestions(std::string partialWord);
+};
+
+#endif
